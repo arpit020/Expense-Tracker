0b6463543a52b60e3cec89df40e72e67c2b77188
arpit020 <rathiarpit29@gmail.com>
2025-04-04 23:00:01 +0530

    User Service
    User Service to add new user

---
+++ UserService/src/main/java/comExpenseTracker/UserService/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package comExpenseTracker.UserService.repository;
+
+import comExpenseTracker.UserService.entities.UserInfo;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@EnableJpaRepositories
+public interface UserRepository extends CrudRepository<UserInfo, String>
+{
+
+    Optional<UserInfo> findByUserId(String userId);
+
+}
